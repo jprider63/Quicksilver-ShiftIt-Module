843f9700901a54a5f3000dfa2bf4b19e09164205
jp.rider63 <dev@jamesparker.me>
2012-03-16 18:49:04 -0400

    Actually added trigger controls.

--- ShiftIt_Module/ShiftIt_Module/QSShiftItControlProvider.h
+++ ShiftIt_Module/ShiftIt_Module/QSShiftItControlProvider.h
@@ -3,11 +3,20 @@
 //  ShiftIt Module
 //
 //  Created by James Parker on 3/9/12.
-//  Copyright (c) 2012 university of md college park. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
-#import <QSCore/QSCore.h>
+//#import <QSCore/QSCore.h>
 
 @interface QSShiftItControlProvider : QSActionProvider
-
+- (void)left;
+- (void)right;
+- (void)top;
+- (void)bottom;
+- (void)topLeft;
+- (void)topRight;
+- (void)bottomLeft;
+- (void)bottomRight;
+- (void)center;
+- (void)fullscreen;
 @end
--- ShiftIt_Module/ShiftIt_Module/QSShiftItControlProvider.m
+++ ShiftIt_Module/ShiftIt_Module/QSShiftItControlProvider.m
@@ -3,11 +3,51 @@
 //  ShiftIt Module
 //
 //  Created by James Parker on 3/9/12.
-//  Copyright (c) 2012 university of md college park. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "QSShiftItControlProvider.h"
 
 @implementation QSShiftItControlProvider
+
+- (void)left {
+    NSBeep();
+}
+
+- (void)right {
+    
+}
+
+- (void)top {
+    
+}
+
+- (void)bottom {
+    
+}
+
+- (void)topLeft {
+    
+}
+
+- (void)topRight {
+    
+}
+
+- (void)bottomLeft {
+    
+}
+
+- (void)bottomRight {
+    
+}
+
+- (void)center {
+    
+}
+
+- (void)fullscreen {
+    
+}
 
 @end
